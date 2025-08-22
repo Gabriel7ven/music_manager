fa48bf3cf956f215730dbe0c92b4ec3d5f16da5a
Gabriel7ven <gabriel.oficial@yahoo.com>
2025-08-22 16:55:01 -0300

    Atualization

--- schedules/forms.py
+++ schedules/forms.py
@@ -6,7 +6,7 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    # cc_myself = forms.BooleanField(required=False)
     
     
 class MusicForm(forms.Form):

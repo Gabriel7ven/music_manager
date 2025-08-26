e5cdfe10d0ae160c0284d89aba23dbbb0cbe3d83
Gabriel7ven <gabriel.oficial@yahoo.com>
2025-08-26 17:53:29 -0300

    Adding javascript

---
+++ schedules/static/schedules/app.js
@@ -0,0 +1,11 @@
+const btnCopiarCronograma = document.getElementById('copiar-cronograma');
+
+btnCopiarCronograma.addEventListener('click', function() {
+    this.style.backgroundColor = 'yellow';
+})
+
+function copiarCronograma() {
+    
+
+
+}
